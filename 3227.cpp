6a9596d3a1dec020cfcd2a260cdfffeb4e643d6f
HimaniParik <himaniparik01@gmail.com>
2025-09-12 21:57:25 +0530

    Added solution for problem 3277

---
+++ 3227.cpp
@@ -0,0 +1,13 @@
+ bool doesAliceWin(string s) {
+      
+       for(char c:s)
+        {
+            if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u')
+            {
+                return true;
+                
+            }
+        }
+        return false;
+        
+    }
